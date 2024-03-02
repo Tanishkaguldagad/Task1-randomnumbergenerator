4146284ded73f9e610dec855ed0a61b832f49d3b
gaura <gaura@Siya>
2024-03-03 03:23:37 +0530

    first commit

---
+++ src/randomnumber/randomnumbergenerator.java
@@ -0,0 +1,12 @@
+package randomnumber;
+
+public class randomnumbergenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double random=Math.random();
+int range=100;
+System.out.println(random*range);
+	}
+
+}
